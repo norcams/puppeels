8fc4ec65038f49209295c6443473a6f7d6e1e184
edpto <tor.ladre@it.uib.no>
2015-02-05 10:14:51 +0100

    Added basic module for managing network interfaces with netcf

---
+++ manifests/base/interfaces.pp
@@ -0,0 +1,15 @@
+# Class: profile::base::interfaces
+#
+#
+class profile::base::interfaces {
+  $network_params = hiera_hash (networkInterfaces, undef)
+  unless empty(network_params) {
+    create_resources(network_if, $network_params)
+  }
+
+# netcf-puppet needs netcf package
+# The package has the same name on RedHat and debian
+  package { 'netcf':
+    ensure => installed,
+  }
+}
