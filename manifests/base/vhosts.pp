c2c03c06610d815201e10351f489a11269f244a5
edpto <tor.ladre@it.uib.no>
2015-02-19 13:57:05 +0100

    Moved vhost management to profile webserver

--- manifests/base/vhosts.pp
+++
@@ -1,9 +0,0 @@
-# Class: profile::base::vhosts
-#
-#
-class profile::base::vhosts {
-  $vhosts_params = hiera_hash (definedVhosts, undef)
-  unless empty(vhosts_params) {
-    create_resources(apache::vhost, $vhosts_params)
-  }
-}
