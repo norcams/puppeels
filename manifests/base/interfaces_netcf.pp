d32a806894c43de71738dd9825ed6749d2b6d4b0
edpto <tor.ladre@it.uib.no>
2015-02-23 19:09:17 +0100

    Fixed interfaces classes

--- manifests/base/interfaces_netcf.pp
+++ manifests/base/interfaces_netcf.pp
@@ -3,7 +3,11 @@
 #
 class profile::base::interfaces_netcf {
   $network_params_netcf = hiera_hash (networkInterfacesNetcf, undef)
-  unless empty(network_params_netcf) {
+  unless empty($network_params_netcf) {
     create_resources(netcf_if, $network_params_netcf)
   }
 
+# netcf-puppet needs netcf package
+# The package has the same name on RedHat and debian
+  ensure_resource('package', 'netcf', {'ensure' => 'installed'})
+}
