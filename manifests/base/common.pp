19a5d1658d3a01eba05cdb9ed5ee655ce7258fa0
edpto <tor.ladre@it.uib.no>
2015-03-09 20:30:27 +0100

    Added profile for management of logical volumes

--- manifests/base/common.pp
+++ manifests/base/common.pp
@@ -40,6 +40,7 @@ class profile::base::common (
   $manage_authconfig      = false,
   $manage_firewall        = false,
   $manage_networkifs      = false,
+  $manage_lvm             = false,
   $manage_timezones	  = false,
   $common_packages        = [],
   $common_packages_ensure = 'installed',
@@ -97,6 +98,10 @@ class profile::base::common (
 
   if $manage_networkifs {
     include ::profile::base::interfaces
+  }
+
+  if $manage_lvm {
+    include ::profile::base::lvm
   }
 
   if $manage_timezones {
