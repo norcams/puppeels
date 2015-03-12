4fc6c65a42017a411c2d28daf0cc2a6153637335
edpto <tor.ladre@it.uib.no>
2015-03-12 16:58:21 +0100

    Added possibility for creating quick lvm and fs resources

--- manifests/base/lvm.pp
+++ manifests/base/lvm.pp
@@ -3,4 +3,9 @@
 #
 class profile::base::lvm {
   include ::lvm
+
+  create_resources(physical_volume, hiera_hash('physical_volume', {}))
+  create_resources(volume_group, hiera_hash('volume_group', {}))
+  create_resources(logical_volume, hiera_hash('logical_volume', {}))
+  create_resources(filesystem, hiera_hash('filesystem', {}))
 }
