d8afc419ffea30101051211cfbd8bf060c4230b9
Jan Ivar Beddari <code@beddari.net>
2015-04-13 08:45:09 +0200

    Managing firewall in Foreman profile

--- manifests/virtualization/libvirt.pp
+++ manifests/virtualization/libvirt.pp
@@ -1,8 +1,13 @@
+#
 # class profile::virtualization::libvirt
 #
-#
-
-class profile::virtualization::libvirt {
+class profile::virtualization::libvirt(
+  $manage_firewall = false,
+  $firewall_extras = {
+    'tcp' => {},
+    'tls' => {},
+  },
+) {
   include ::libvirt
 
   $libvirt_networks = hiera_hash (libvirt_networks, undef)
@@ -13,5 +18,17 @@ class profile::virtualization::libvirt {
   $libvirt_pool = hiera_hash (libvirt_pool, undef)
   unless empty($libvirt_pool) {
     create_resources(libvirt_pool, $libvirt_pool)
+  }
+
+  if $manage_firewall {
+    profile::firewall::rule { '180 libvirt-tcp accept tcp':
+      port   => 16509,
+      extras => $firewall_extras['tcp']
+    }
+
+    profile::firewall::rule { '181 libvirt-tls accept tcp':
+      port   => 16514,
+      extras => $firewall_extras['tls']
+    }
   }
 }
