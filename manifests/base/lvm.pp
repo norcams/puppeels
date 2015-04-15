d7396691ba03d182e6b89aab5943915709e25a7a
Jan Ivar Beddari <code@beddari.net>
2015-04-15 15:20:04 +0200

    FIX: Properly address defined types

--- manifests/base/lvm.pp
+++ manifests/base/lvm.pp
@@ -5,7 +5,7 @@ class profile::base::lvm {
   include ::lvm
 
   create_resources(physical_volume, hiera_hash('physical_volume', {}))
-  create_resources(volume_group, hiera_hash('volume_group', {}))
-  create_resources(logical_volume, hiera_hash('logical_volume', {}))
+  create_resources('::lvm::volume_group', hiera_hash('lvm::volume_group', {}))
+  create_resources('::lvm::logical_volume', hiera_hash('lvm::logical_volume', {}))
   create_resources(filesystem, hiera_hash('filesystem', {}))
 }
