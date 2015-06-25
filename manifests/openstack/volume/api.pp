7849dfb5e923584c3343dd247fd9390e92ed2c3f
edpto <tor.ladre@it.uib.no>
2015-06-25 17:29:43 +0200

    Added firewall rule for cinder API

--- manifests/openstack/volume/api.pp
+++ manifests/openstack/volume/api.pp
@@ -1,7 +1,16 @@
-class profile::openstack::volume::api {
+class profile::openstack::volume::api(
+  $manage_firewall = true,
+  $firewall_extras = {},
+){
   include profile::openstack::volume
 
   include ::cinder::api
   include ::cinder::glance
 
+  if $manage_firewall {
+        profile::firewall::rule{ '8776 cinder-api accept tcp':
+          port   => 8776,
+          extras => $firewall_extras,
+    }
+  }
 }
