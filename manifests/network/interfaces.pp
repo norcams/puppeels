5b675ac44150ec33045deab60d726263d0690ccb
Jan Ivar Beddari <code@beddari.net>
2015-09-30 09:54:11 +0200

    Add named_interfaces to base profile

--- manifests/network/interfaces.pp
+++ manifests/network/interfaces.pp
@@ -1,19 +1,26 @@
 # Class: profile::base::interfaces
 #
 #
-class profile::base::interfaces {
+class profile::network::interfaces {
+
+  # Set up extra logical fact names for network facts
+  include named_interfaces
+
   $network_params = hiera_hash (network_if, undef)
+
+  # netcf-puppet needs netcf package
+  # The package has the same name on RedHat and debian
+
   unless empty($network_params) {
+    ensure_resource('package', 'netcf', {'ensure' => 'installed'})
     create_resources(network_if, $network_params)
   }
 
   $network_params_netcf = hiera_hash (netcf_if, undef)
   unless empty($network_params_netcf) {
+    ensure_resource('package', 'netcf', {'ensure' => 'installed'})
     create_resources(netcf_if, $network_params_netcf)
   }
 
-# netcf-puppet needs netcf package
-# The package has the same name on RedHat and debian
-  ensure_resource('package', 'netcf', {'ensure' => 'installed'})
 }
 
