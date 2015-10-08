bbbbe659f784fc1179d46d91300cd3973fe5144f
edpto <tor.ladre@it.uib.no>
2015-10-08 16:50:51 +0200

    Added firewall rule to new horizon interface

--- manifests/openstack/dashboard.pp
+++ manifests/openstack/dashboard.pp
@@ -6,8 +6,10 @@ class profile::openstack::dashboard(
 
   if $manage_firewall {
     profile::firewall::rule { '235 openstack-dashboard accept tcp':
-      port   => [80,443],
-      extras => $firewall_extras
+      port    => [80,443],
+      iniface => $interface_service1,
+      source  => '129.177.0.0/16',
+      extras  => $firewall_extras
     }
   }
 }
