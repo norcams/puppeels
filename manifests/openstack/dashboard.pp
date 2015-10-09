5777122c1311d483c22d5abe0e529a0f1f4c074d
Jan Ivar Beddari <code@beddari.net>
2015-10-09 15:58:38 +0200

    Order certs before horizon service

--- manifests/openstack/dashboard.pp
+++ manifests/openstack/dashboard.pp
@@ -6,18 +6,16 @@ class profile::openstack::dashboard(
   include ::horizon
 
   if $manage_ssl_cert {
-    include profile::application::sslcert
-    Class ['horizon'] ->
-    Class ['profile::application::sslcert'] ->
-    Openssl::Certificate::X509
+    contain profile::application::sslcert
+    Class['Profile::Application::Sslcert'] ~>
+    Service[$::horizon::params::http_service]
   }
 
   if $manage_firewall {
     profile::firewall::rule { '235 openstack-dashboard accept tcp':
       port    => [80,443],
-      iniface => $interface_service1,
-      source  => '129.177.0.0/16',
-      extras  => $firewall_extras
+      iniface => "$interface_service1",
+      extras  => $firewall_extras,
     }
   }
 }
