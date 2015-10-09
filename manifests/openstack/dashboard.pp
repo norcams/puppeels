097a346ba0f685110dc35db3379eb56c1ae99732
edpto <tor.ladre@it.uib.no>
2015-10-09 13:42:16 +0200

    Preliminary ssl support for openstack horizon

--- manifests/openstack/dashboard.pp
+++ manifests/openstack/dashboard.pp
@@ -1,8 +1,16 @@
 class profile::openstack::dashboard(
+  $manage_ssl_cert = true,
   $manage_firewall = true,
   $firewall_extras = {},
 ) {
   include ::horizon
+
+  if $manage_ssl_cert {
+    include profile::application::sslcert
+    Class ['horizon'] ->
+    Class ['profile::application::sslcert'] ->
+    Openssl::Certificate::X509
+  }
 
   if $manage_firewall {
     profile::firewall::rule { '235 openstack-dashboard accept tcp':
