170dcbfc71175cb9a74b5b97da020b0af39e6b90
edpto <tor.ladre@it.uib.no>
2015-10-14 12:28:05 +0200

    Renamed parameters for clarity

--- manifests/base/lvm.pp
+++ manifests/base/lvm.pp
@@ -4,8 +4,8 @@
 class profile::base::lvm {
   include ::lvm
 
-  create_resources(physical_volume, hiera_hash('physical_volume', {}))
-  create_resources(volume_group, hiera_hash('volume_group', {}))
-  create_resources(logical_volume, hiera_hash('logical_volume', {}))
-  create_resources(filesystem, hiera_hash('filesystem', {}))
+  create_resources(physical_volume, hiera_hash('profile::base::lvm::physical_volume', {}))
+  create_resources(volume_group, hiera_hash('profile::base::lvm::volume_group', {}))
+  create_resources(logical_volume, hiera_hash('profile::base::lvm::logical_volume', {}))
+  create_resources(filesystem, hiera_hash('profile::base::lvm::filesystem', {}))
 }
