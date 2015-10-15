fff7da47a5341f87e20183f8055e14221183b839
edpto <tor.ladre@it.uib.no>
2015-10-15 10:07:37 +0200

    Fixed creation of logical volumes

--- manifests/base/lvm.pp
+++ manifests/base/lvm.pp
@@ -4,8 +4,8 @@
 class profile::base::lvm {
   include ::lvm
 
-  create_resources(physical_volume, hiera_hash('profile::base::lvm::physical_volume', {}))
-  create_resources(volume_group, hiera_hash('profile::base::lvm::volume_group', {}))
-  create_resources(logical_volume, hiera_hash('profile::base::lvm::logical_volume', {}))
-  create_resources(filesystem, hiera_hash('profile::base::lvm::filesystem', {}))
+  create_resources(physical_volume, hiera('profile::base::lvm::physical_volume', {}))
+  create_resources(volume_group, hiera('profile::base::lvm::volume_group', {}))
+  create_resources(lvm::logical_volume, hiera('profile::base::lvm::logical_volume', {}))
+  create_resources(filesystem, hiera('profile::base::lvm::filesystem', {}))
 }
