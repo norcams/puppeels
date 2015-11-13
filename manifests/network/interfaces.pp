e58474df7e92eb72f4c80f7eb958f00a75e6898e
Jan Ivar Beddari <code@beddari.net>
2015-11-13 13:48:00 +0100

    Manage SFP option for Intel 10G cards

--- manifests/network/interfaces.pp
+++ manifests/network/interfaces.pp
@@ -24,4 +24,21 @@ class profile::network::interfaces {
 
   # New example42 network module
   include ::network
+
+  # Configure 82599ES SFP+ interface module options, if present
+  if $::lspci_has['intel82599sfp'] and "ixgbe" in $::kernel_modules {
+    include ::kmod
+    # Set SFP option in /etc/modprobe.d/ixgbe.conf
+    kmod::option { 'allow all SFPs':
+      module  => 'ixgbe',
+      option  => 'allow_unsupported_sfp',
+      value   => '1',
+    }
+    # Set option in grub2
+    kernel_parameter { "ixgbe.allow_unsupported_sfp":
+      ensure => present,
+      value  => "1",
+    }
+  }
+
 }
