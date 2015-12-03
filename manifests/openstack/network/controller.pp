047d6629e4e387cc553e1c25fb7eb0b4d72f608f
edpto <tor.ladre@it.uib.no>
2015-12-03 19:29:37 +0100

    Added function for setting neutron network policies from hash

--- manifests/openstack/network/controller.pp
+++ manifests/openstack/network/controller.pp
@@ -1,4 +1,6 @@
 class profile::openstack::network::controller(
+  $manage_neutron_policies = true,
+  $neutron_policy_path = '/etc/neutron/policy.json',
   $manage_firewall = true,
   $firewall_extras = {}
 ) {
@@ -6,6 +8,14 @@ class profile::openstack::network::controller(
 
   include ::neutron::server
   include ::neutron::server::notifications
+
+  if $manage_neutron_policies {
+    Openstacklib::Policy::Base {
+      file_path => $neutron_policy_path,
+    }
+    create_resources('openstacklib::policy::base', hiera('profile::openstack::network::policies', {}))
+  }
+
   if $manage_firewall {
     profile::firewall::rule { '210 neutron-server accept tcp':
       port   => 9696,
