76977a76a3e36af9962b5c0fccd16428b2d8634c
edpto <tor.ladre@it.uib.no>
2015-12-15 16:13:58 +0100

    As a temporary solution, the calico dhcp agent will be installed with the compute profile

--- manifests/openstack/compute.pp
+++ manifests/openstack/compute.pp
@@ -2,3 +2,12 @@ class profile::openstack::compute {
   include ::nova
   include ::nova::network::neutron
 }
+
+# All compute hosts need this dhcp agent
+# which should have been a dependeny for the
+# calico-compute package. This may be fixed
+# in the future.
+
+package { 'networking-calico':
+  ensure => installed,
+}
