2cf06dc891af6f05dd7d60dc7f110327fbcb9f56
edpto <tor.ladre@it.uib.no>
2015-12-16 09:51:05 +0100

    Removed package installation

--- manifests/openstack/compute.pp
+++ manifests/openstack/compute.pp
@@ -2,12 +2,3 @@ class profile::openstack::compute {
   include ::nova
   include ::nova::network::neutron
 }
-
-# All compute hosts need this dhcp agent
-# which should have been a dependeny for the
-# calico-compute package. This may be fixed
-# in the future.
-
-package { 'networking-calico':
-  ensure => installed,
-}
